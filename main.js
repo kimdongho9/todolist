fe5a5cbd71957125b48bb7ef1c81c370b758f9c7
kimdongho9 <guzzi9393@gamil.com>
2024-02-21 21:40:50 +0900

    add

--- main.js
+++ main.js
@@ -16,26 +16,31 @@ let underLine = document.getElementById("underline");
 let taskList = [];
 let mode = "all";
 let filterList = [];
-let doneList = [];
-
 
 
 taskInput.addEventListener("keyup", validate); //input 입력시 add버튼 활성화
 
-
+// 처음부터 underline이 'all' 에 위치할 수 있도록 하는 함수
+window.onload = function () {
+  let initialTab = document.getElementById("all");
+  underLine.style.opacity = "1";
+  underLine.style.transition = "0.4s ease-in";
+  underLine.style.top = "0";
+  underLine.style.left = initialTab.offsetLeft + "px";
+  underLine.style.width = initialTab.offsetWidth + "px";
+  underLine.style.height = initialTab.offsetHeight + "px";
+};
 
 // 언더바 이벤트
 tabs.forEach((menu) =>
   menu.addEventListener("click", (e) => underlineIndicator(e))
 );
 
-
-
 // enter 입력이벤트
 taskInput.addEventListener("keyup", (e) => {
   if (e.keyCode === 13) {
-    e.preventDefault(); // 기존의 enter의 효과무효화, enter자체만 누른걸로 만들어줌
-    addButton.click(); // 하단 add버튼의 click 실행되는 것과 같게 해줌
+    e.preventDefault(); // 기본 엔터 키 동작을 무효화하여 새로운 줄이 추가되지 않도록 함
+    addButton.click(); //  "add" 버튼 클릭 이벤트를 호출하여 엔터 키를 눌러도 클릭 된 것과 같게 함.
   }
 });
 
@@ -50,21 +55,25 @@ for (let i = 1; i <= tabs.length; i++) {
 }
 
 
-console.log(tabs);
 // add버튼 클릭 후 함수
 
 function addTask() {
   let task = {
-    taskContent: taskInput.value,
+    taskContent: taskInput.value, //등록 할 할 일
     isComplete: false,
     id: randomIDGenerator(),
   };
+
   taskList.push(task);
-  console.log(taskList);
+
   render();
-  taskInput.value = "";
   validate();
 }
+
+
+
+
+
 
 
 function render() {
@@ -73,36 +82,43 @@ function render() {
   // all -> taskList / ongoing, done -> filterList
   let list = [];
 
-  
   if (mode === "all") {
     list = taskList;
   } else if (mode === "ongoing" || mode === "done") {
     list = filterList;
   }
 
+  
   let resultHTML = "";
 
   //할 일 생성했을 시, 나타내는 Tab
-
 
   for (let i = 0; i < list.length; i++) {
     //체크버튼 클릭했을 때
     if (list[i].isComplete === true) {
       resultHTML += `<div class="task task-done">
-                  <div class="task-done">${list[i].taskContent}
-                  </div>
-                  <div class="button-box">
+                  <div class ="check-box">
                     <button onclick="toggleComplete('${list[i].id}')"><i class="fas fa-undo-alt"></i></button>
+                    <div class="task-done">${list[i].taskContent}
+                  </div>
+                  </div>
+
+                  <div class="button-box">
+                    <button onclick="editTask('${list[i].id}')"><i class="fa fa-light fa-pencil"></i></button>
                     <button onclick="deleteTask('${list[i].id}')"><i class="fa fa-trash"></i></button>
                   </div>
                   </div>`;
     } else {
       //체크버튼 클릭하지 않았을 때
-      resultHTML += `<div class= "task">
+      resultHTML += 
+      `<div class= "task">
+      <div class ="check-box">
+      <button onclick="toggleComplete('${list[i].id}')"><i class="fa fa-duotone fa-check"></i></button>
       <div>${list[i].taskContent}
       </div>
+      </div>
       <div class="button-box">
-        <button onclick="toggleComplete('${list[i].id}')"><i class="fa fa-solid fa-check" style="color: #63E6BE;"></i></button>
+        <button onclick="editTask('${list[i].id}')"><i class="fa fa-light fa-pencil"></i></button>
         <button onclick="deleteTask('${list[i].id}')"><i class="fa fa-trash"></i></button>
       </div>
       </div>`;
@@ -128,13 +144,7 @@ function toggleComplete(id) {
   render();
 }
 
-
-
-
-
-
 function deleteTask(id) {
-
 
   // all내에 id를 찾아 삭제
   if (mode === "all") {
@@ -146,9 +156,6 @@ function deleteTask(id) {
     }
     console.log(taskList);
     render();
-
-
-
   } else if (mode === "ongoing" || mode === "done") {
     for (let i = 0; i < filterList.length; i++) {
       if (filterList[i].id === id) {
@@ -169,6 +176,7 @@ function deleteTask(id) {
   // console.log(taskList); // 잘 동작하는지 console.log에서 체크
 }
 
+
 //filter 함수
 function filter(event) {
   // console.log("filter", event.target.id); tabs의 모두, 진행중, 끝남 클릭 시 id 잘 들어오는지 확인
@@ -179,10 +187,10 @@ function filter(event) {
   // ongoing일때 -> 진행중인 items를 보여준다 -> task.isComplete = false
   // done일때 -> 끝난 items를 보여준다 -> task.isComplete = true
 
-
   // 모두를 클릭시 나타나는 로직
   if (mode === "all") {
     render();
+
     //진행중 클릭시 나타나는 로직
   } else if (mode === "ongoing") {
     for (let i = 0; i < taskList.length; i++) {
@@ -192,8 +200,7 @@ function filter(event) {
     }
 
     render();
-    console.log(filterList);
-  
+
     //끝남 클릭시 나타나는 로직
   } else if (mode === "done") {
     for (let i = 0; i < taskList.length; i++) {
@@ -202,98 +209,35 @@ function filter(event) {
       }
     }
     render();
-  console.log(filterList);
   }
-}
 
+  underlineIndicator(e); //underLine
+}
 
 // input입력시 add버튼 활성여부 함수
 function validate() {
-  if (taskInput.value === "") {
-  addButton.disabled = true;
+  if (taskInput.value == "") {
+    addButton.disabled = true;
+    alert("입력 값이 없습니다!");
   } else {
-  addButton.disabled = false;
+    addButton.disabled = false;
   }
 }
 
 
-// under-line 이동하는 함수
+
+// under-line 모양
 function underlineIndicator(e) {
   underLine.style.left = e.currentTarget.offsetLeft + "px";
   underLine.style.width = e.currentTarget.offsetWidth + "px";
-  underLine.style.top = e.currentTarget.offsetHeight + "px";
+  underLine.style.height = e.currentTarget.offsetHeight + "px";
+  underLine.style.opacity = "1";
+  underLine.style.transition = "0.4s ease-in";
+  underLine.style.top = "0";
 }
-
 
 //랜덤 Id 생성기
 function randomIDGenerator() {
   return "_" + Math.random().toString(36).substr(2, 9);
 }
 
-
-
-
-
-// 현재 날짜를 가져오는 함수
-function getCurrentDate() {
-  const currentDate = new Date();
-  const options = {
-    month: 'short',
-    day: '2-digit',
-    weekday: 'short'
-  };
-  const dateStr = currentDate.toLocaleDateString('ko-KR', options);
-  return dateStr;
-}
-
-
-// HTML 요소에 현재 날짜와 요일을 추가하는 함수
-function displayCurrentDate() {
-  const currentDate = getCurrentDate();
-  const weekDay = currentDate.slice(0, 3);
-  const date = currentDate.slice(4);
-  const year = new Date().getFullYear(); // 연도 가져오기
-
-  
-  // HTML 요소 선택
-  let todayDate = document.querySelector('.today-date');
-  let todayWeekday = document.querySelector('.today-weekday');
-  let dateHTML = '';
-
-  todayWeekday.textContent = weekDay;
-
-  // 생성된 HTML을 화면에 추가
-  dateHTML += `
-      <div class="date-area">
-      <section class="date-container">
-        <p class="day">${date.slice(5)}</p>
-        <div class="day-year">
-        <p class="month">${date.slice(1,4)}</p>
-        <p class="year">${year}</p>
-        </div>
-      </section>
-      </div>
-      `;
-
-  // todayDate 요소에 생성된 HTML 추가
-
-  todayDate.innerHTML = dateHTML;
-  console.log("Date:", date);
-  console.log("Day:", weekDay);
-}
-
-displayCurrentDate();
-
-
-
-// // 시작 버튼 클릭 시 이벤트
-// startBtn.addEventListener('click', function () {
-//   startBtn.classList.add('hidden'); // 시작 버튼을 숨깁니다.
-//   setTimeout(() => {
-//     startBtn.style.display = 'none'; // 시작 버튼을 완전히 숨깁니다.
-//     setTimeout(function () {
-//       inputArea.classList.add('visible'); // 입력 영역을 보이게 합니다.
-//     })
-//   }, 300);
-//   taskInput.focus(); // 할일 입력란에 포커스를 설정합니다.
-// });
